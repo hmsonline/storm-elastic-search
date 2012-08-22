0bb376a0fa8ff3c4efbc1c066714b975858fd9ad
Brian O'Neill <bone@alumni.brown.edu>
2012-08-22 12:35:25 -0400

    Unit test for elastic.

--- src/test/java/com/hmsonline/storm/contrib/bolt/elasticsearch/ElasticSearchTest.java
+++ src/test/java/com/hmsonline/storm/contrib/bolt/elasticsearch/ElasticSearchTest.java
@@ -1,29 +1,21 @@
 package com.hmsonline.storm.contrib.bolt.elasticsearch;
 
-import org.elasticsearch.action.index.IndexResponse;
+import static org.elasticsearch.node.NodeBuilder.nodeBuilder;
+
 import org.elasticsearch.client.Client;
-import org.elasticsearch.client.transport.TransportClient;
-import org.elasticsearch.common.settings.ImmutableSettings;
-import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.node.Node;
+import org.junit.Test;
 
 public class ElasticSearchTest {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		Settings settings = ImmutableSettings.settingsBuilder()
-                .put("cluster.name", "dlcirrus elastic cluster").build();
-
-		Client client = new TransportClient(settings).addTransportAddress(new InetSocketTransportAddress("dlcirrus03", 9300));
-		
-		String id = "1234";
-		String json = "{\"foo\":\"bar\"}";
-		
-		IndexResponse response = client.prepareIndex("test", "entity", id).setSource(json.getBytes()).execute().actionGet();
-	}
+    @Test
+    public void testIndexing() {
+        Node node = nodeBuilder().node();
+        Client client = node.client();
+        
+        String id = "1234";
+        String json = "{\"foo\":\"bar\"}";
+        client.prepareIndex("test", "entity", id).setSource(json.getBytes()).execute().actionGet();
+    }
 
 }
